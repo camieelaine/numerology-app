2b9c21bccb83dc495719f20c675c990296095db6
Camie DeCovich <camiedecovich@gmail.com>
2015-07-29 14:01:21 -0800

    CRUD, Partials and Helpers, Error Handling Changes
    Changes include adding partials and helpers, error handling, and CRUD
    with forms.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,16 +1,14 @@
-#Controller to show all the people
+#Method to show all person records
 get '/people' do
   @people = Person.all
   erb :"/people/index"
 end
-
-#Controller to show the form to add a new person record
+#Method to show the new person record form
 get '/people/new' do
   @person = Person.new
   erb :"/people/new"
 end
-
-#Controller to add a new person record to the database
+#Method to add a person record to the database
 post '/people' do
   if params[:birthdate].include?("-")
     birthdate = params[:birthdate]
@@ -19,33 +17,44 @@ post '/people' do
   end
   
   person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
-  redirect "/people/#{person.id}"
+  if @person.valid?
+    @person.save
+    redirect "/people/#{@person.id}"
+  else
+    @person.errors.full_messages.each do |msg|
+      @errors = "#{@errors} #{msg}."
+    end
+    erb :"/people/new"
+  end
 end
-
-#Controller to show the form to edit a person record
+#Method to show a person record to edit
 get '/people/:id/edit' do
   @person = Person.find(params[:id])
   erb :"/people/edit"
 end
-
-#Controller to update a person record in the database
+#Method to update a person record
 put '/people/:id' do
-  person = Person.find(params[:id])
-  person.first_name = params[:first_name]
-  person.last_name = params[:last_name]
-  person.birthdate = params[:birthdate]
-  person.save
-  redirect "/people/#{person.id}"
+  @person = Person.find(params[:id])
+  @person.first_name = params[:first_name]
+  @person.last_name = params[:last_name]
+  @person.birthdate = params[:birthdate]
+  if @person.valid?
+    @person.save
+    redirect "/people/#{@person.id}"
+  else
+    @person.errors.full_messages.each do |msg|
+      @errors = "#{@errors} #{msg}."
+    end
+    erb :"/people/edit"    
+  end
 end
-
-#Controller to delete a person record in the database
+#Method to delete a person record
 delete '/people/:id' do
   person = Person.find(params[:id])
   person.delete
   redirect "/people"
 end
-
-#Controller to retrieve a person record from the database and show the record
+#Method to show a person record
 get '/people/:id' do
   @person = Person.find(params[:id])
   birth_path_num = Person.get_birth_path_num(@person.birthdate.strftime("%m%d%Y"))
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,41 @@
 module ApplicationHelper
+#Helper method called people_form_action which takes in a person as an argument, then checks if the person 
+#is a new record.
+#Set a variable @action to the appropriate route for each case of the if statement.
+	def people_form_action(person)
+		if person.new_record?
+			@action = "/people"
+		else
+			@action = "/people/#{person.id}"
+		end
+		@action
+	end
+
+	#Set hidden _method value in form to post or put
+	def people_form_method(person)
+		if person.new_record?
+			@method = "post"
+		else
+			@method = "put"
+		end
+		@method
+	end
+#Define which css class to use
+	def people_form_class(person)
+	    if person.new_record?
+	      @class = "new-person"
+	    else
+	      @class = "edit-person"
+	    end
+	    @class
+	end
+	
+	def people_form_id(person)
+	    if person.new_record?
+	      @form_id = "new-person"
+	    else
+	      @form_id = "person-#{person.id}"
+	    end
+	    @form_id
+	 end
 end
